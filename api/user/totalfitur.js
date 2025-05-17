0ffc51aeae167b7bfc1fc95d5d81cfc4d0b6fe26
shinzxyz <shinzx723@gmail.com>
2025-05-18 04:36:59 +0700

    update

--- api/user/totalfitur.js
+++ api/user/totalfitur.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 module.exports = {
   name: 'Total Api',
-  desc: 'Menampilkan total semua fitur dari semua kategori',
+  desc: 'Menampilkan total semua fitur di semua kategori',
   category: 'User',
   async run(req, res) {
     try {
-      const baseDir = path.join(__dirname, '../..'); // naik ke folder 'api'
+      const baseDir = path.join(__dirname, '..'); // naik ke folder 'api'
       const categories = fs.readdirSync(baseDir).filter(file => {
         const fullPath = path.join(baseDir, file);
         return fs.statSync(fullPath).isDirectory();
